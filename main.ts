8aaba1d033d09441486155930f2595ced8484b12
TCSDragon <56455034+TCSDragon@users.noreply.github.com>
2023-06-28 21:37:27 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-basic.showLeds(`
-    # . . . .
-    . # . . .
-    . . # . .
-    . . . # .
-    . . . . #
-    `)
 basic.forever(function () {
-	
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.clearScreen()
+    basic.pause(100)
 })
